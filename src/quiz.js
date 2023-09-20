56989b9d370441e7948eab64134bd88cead468c2
Steven Xu <z5481988@ad.unsw.edu.au>
2023-09-20 18:31:04 +1000

    Finished adminQuizInfo and adminQuizNameUpdate

---
+++ src/quiz.js
@@ -0,0 +1,40 @@
+/*
+Parameters:
+   ( authUserId, quizId )
+
+Return object:
+{
+    quizId: 1,
+    name: 'My Quiz',
+    timeCreated: 1683125870,
+    timeLastEdited: 1683125871,
+    description: 'This is my quiz',
+  }
+
+Gets and returns all of the relevant information about the current quiz.
+*/
+
+function adminQuizInfo(authUserId, quizId) {
+    return {
+        quizId: 1,
+        name: 'My Quiz',
+        timeCreated: 1683125870,
+        timeLastEdited: 1683125871,
+        description: 'This is my quiz',       
+    }
+}
+
+/*
+Parameters:
+      ( authUserId, quizId, name )
+      
+Return object:
+      { } empty object
+
+Updates the name of the relevant quiz.
+*/
+function adminQuizNameUpdate(authUserId, quizId, name) {
+    return {
+        
+    }
+}
