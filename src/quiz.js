24b56d77aaba9b250bc7b74029677aa8379c5d73
Steven Xu <z5481988@ad.unsw.edu.au>
2023-09-20 18:42:16 +1000

    Finished admonQuizNameUpdate

---
+++ src/quiz.js
@@ -0,0 +1,14 @@
+/*
+Parameters:
+      ( authUserId, quizId, name )
+      
+Return object:
+      { } empty object
+
+Updates the name of the relevant quiz.
+*/
+function adminQuizNameUpdate(authUserId, quizId, name) {
+    return {
+        
+    }
+}
