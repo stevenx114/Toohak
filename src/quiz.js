d4760b4eca83bcf563ab8b9ba51dda95228b9202
Varshan Pranavendran <z5418456@ad.unsw.edu.au>
2023-09-20 19:00:52 +1000

    Revert "Merge branch 'it0/SX/adminQuizNameUpdate/v0' into 'master'"
    This reverts merge request !6

--- src/quiz.js
+++ src/quiz.js
@@ -1,18 +1,4 @@
 /*
-Parameters:
-      ( authUserId, quizId, name )
-      
-Return object:
-      { } empty object
-
-Updates the name of the relevant quiz.
-*/
-function adminQuizNameUpdate(authUserId, quizId, name) {
-    return {
-        
-    }
-
-/*
  Provide a list of all quizzes that are owned by the currently logged in user.
 
  Input Parameters:
@@ -39,4 +25,5 @@ function adminQuizList(authUserId) {
         }
       ]
     };
+
 }
