9bd2f6db4cf17140bd87ddaee74dbd9503717593
Steven Xu <z5481988@ad.unsw.edu.au>
2023-09-21 01:50:44 +1000

    adminQuizInfo stub function new

--- src/quiz.js
+++ src/quiz.js
@@ -23,32 +23,3 @@ function adminQuizInfo(authUserId, quizId) {
         description: 'This is my quiz',       
     }
 }
-
-/*
- Provide a list of all quizzes that are owned by the currently logged in user.
-
- Input Parameters:
- ( authUserId )
-
- Return: 
-
-{ quizzes: [
-    {
-      quizId: 1,
-      name: 'My Quiz',
-    }
-    ]
- } 
-*/
-
-
-function adminQuizList(authUserId) {
-
-    return { quizzes: [
-        {
-          quizId: 1,
-          name: 'My Quiz',
-        }
-      ]
-    };
-}
