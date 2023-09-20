c76ce6f1664541f1609df58f0e1e3f6788902b16
Tiffany Zhu <z5481408@ad.unsw.edu.au>
2023-09-20 18:17:11 +1000

    function stub for adminQuizRemove

---
+++ src/quiz.js
@@ -0,0 +1,12 @@
+/**
+ * Given a particular quiz, permanently remove the quiz.
+ *
+ * @param {number} authUserId of integers
+ * @param {number} quizId of integers
+ * @returns {object} empty object
+ */
+function adminQuizRemove(authUserId, quizId ) {
+    return { 
+
+    }
+}
