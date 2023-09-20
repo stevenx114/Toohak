47cae2b4e55e8789c88cb2b7767b73670dd0bf7c
Yiru Jang <z5480998@ad.unsw.edu.au>
2023-09-20 18:42:13 +1000

    stub for adminAuthRegister written

---
+++ src/auth.js
@@ -0,0 +1,6 @@
+// Register a user with an email, password, and names, then returns their authUserId value.
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+    return {
+        authUserId: 1,
+    }
+}
