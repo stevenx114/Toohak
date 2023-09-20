abfe4434f2357acb1268708b05afb03327e1fd25
Tiffany Zhu <z5481408@ad.unsw.edu.au>
2023-09-20 18:50:57 +1000

    function stub for adminQuizCreate/v0

---
+++ src/auth.js
@@ -0,0 +1,14 @@
+/**
+ * Given basic details about a new quiz, create one for the logged in user.
+ *
+ * @param {number} authUserId 
+ * @param {string} name 
+ * @param {string} description 
+ * @returns {object} quiz info
+ */
+function adminQuizCreate(authUserId, name, description) {
+    return {
+        quizId: 2,
+    }
+}
+
