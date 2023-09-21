04f20b5588239812e7618edbdf9483854cc1962b
Yiru Jang <z5480998@ad.unsw.edu.au>
2023-09-21 20:57:36 +1000

    added a closing curly bracket at the end of the function

--- src/auth.js
+++ src/auth.js
@@ -3,6 +3,7 @@ function adminAuthLogin(email, password) {
     return {
         authUserId: 1,
     }
+}
 
 // Given an admin user's authUserId, return details about the user.
 // "name" is the first and last name concatenated with a single space between them
