823f22126d63274915cd66909698a98dffc06caf
Varshan Pranavendran <z5418456@ad.unsw.edu.au>
2023-09-30 23:32:13 +1000

    created tests for adminQuizDescriptionUpdate function

--- src/adminQuizDescriptionUpdate.test.js
+++ src/adminQuizDescriptionUpdate.test.js
@@ -1,213 +1,89 @@
 import {
-    clear,
-  } from './other';
+  clear,
+} from './other';
 
-  import {
-    adminAuthRegister,
-    adminAuthLogin,
-    adminUserDetails,
-    adminQuizList,
-    adminQuizCreate,
-    adminQuizRemove,
-    adminQuizInfo,
-    adminQuizNameUpdate,
-    adminQuizDescriptionUpdate
-  } from './auth';
+import {
+  adminAuthRegister,
+  adminQuizCreate,
+  adminQuizDescriptionUpdate
+} from './auth';
   
-  
-  
-  
-  const ERROR = { error: expect.any(String) };
-  
-  beforeEach(() => {
-    clear();
-  });
+const ERROR = { error: expect.any(String) };
+
+let authUserId;
+let quizId;
+
+beforeEach(() => {
+  clear();
+  authUserId = adminAuthRegister('sample@gmail.com', 'samplepassword1', 'firstname', 'lastname');
+  quizId = adminQuizCreate(authUserId.authUserId, "quizName", "sample");
+});
    
-  describe('adminQuizDescriptionUpdate Input Tests', () => {
+describe('adminQuizDescriptionUpdate Test', () => {
 
-    describe('Empty Data Store Test', () => {
+  describe('Empty Data Store Test', () => {
 
-        test('authUserId and QuizId is invalid', () => {
+    test('authUserId and QuizId is invalid', () => {
 
-            // First Create Two Users User
+      clear();
 
-            const email = 'sample@gmail.com';
-            const password = 'samplepassword1';
-            const nameFirst = 'firstname';
-            const nameLast = 'lastname';
-            const authUserId = adminAuthRegister(email, password, nameFirst, nameLast);
-            
-            // Create Quiz
-
-            const quizName = "randomquiz";
-            const newDescription = "random";
-            const quizId = 44;
-
-            
-            expect(adminQuizDescriptionUpdate(authUserId, quizId, newDescription)).toStrictEqual(ERROR);
-
-        });
-
-
+      expect(adminQuizDescriptionUpdate(authUserId.authUserId, quizId.quizId, newDescription)).toStrictEqual(ERROR);
 
     });
-
-    describe('Invalid Input tests', () => {
-
-        test('authUserId is invalid but all other variables are correct', () => {
-
-            // First Create User
-
-            const email = 'sample@gmail.com';
-            const password = 'samplepassword1';
-            const nameFirst = 'firstname';
-            const nameLast = 'lastname';
-            const authUserId = adminAuthRegister(email, password, nameFirst, nameLast);
-            
-            // Create Quiz
-
-            const quizName = "randomquiz";
-            const newDescription = "random";
-            const quizId = adminQuizCreate(authUserId, quizName, "");
-            
-            // Making authUserId invalid
-
-            authUserId += 4;
-
-            
-            expect(adminQuizDescriptionUpdate(authUserId, quizId, newDescription)).toStrictEqual(ERROR);
-            
-        });
-
-
-        test('Invalid quizId but all other variables correct', () => {
-
-            // First Create User
-
-            const email = 'sample@gmail.com';
-            const password = 'samplepassword1';
-            const nameFirst = 'firstname';
-            const nameLast = 'lastname';
-            const authUserId = adminAuthRegister(email, password, nameFirst, nameLast);
-            
-            // Create Quiz
-
-            const quizName = "randomquiz";
-            const newDescription = "random";
-            const quizId = adminQuizCreate(authUserId, quizName, "");
-            
-            // Making quizId invalid
-
-            quizId += 4;
-
-            
-            expect(adminQuizDescriptionUpdate(authUserId, quizId, newDescription)).toStrictEqual(ERROR);
-
-
-        });
-
-        test('given user inputs quizId they dont own but all other variables correct', () => {
-
-            // First Two Users
-
-            const email = 'sample@gmail.com';
-            const password = 'samplepassword1';
-            const nameFirst = 'firstname';
-            const nameLast = 'lastname';
-            const authUserId = adminAuthRegister(email, password, nameFirst, nameLast);
-            const authUserId2 = adminAuthRegister("testing@gmail.com", "badpassword44", "Testing", "testing");
-
-            // Create Quiz with second user
-
-            const quizName = "randomquiz";
-            const newDescription = "newdescription";
-            const quizId = adminQuizCreate(authUserId2, quizName, "");
-            
-
-            // Tries to update quiz user doesnt own 
-            
-            expect(adminQuizDescriptionUpdate(authUserId, quizId, newDescription)).toStrictEqual(ERROR);
-
-        });
-
-
-        test('Invalid Description i.e description.length > 100 characters but all other variables correct', () => {
-
-            
-            // First Create User
-
-            const email = 'sample@gmail.com';
-            const password = 'samplepassword1';
-            const nameFirst = 'firstname';
-            const nameLast = 'lastname';
-            const authUserId = adminAuthRegister(email, password, nameFirst, nameLast);
-            
-            // Create Quiz
-
-            const quizName = "randomquiz";
-            const longDescription = "a".repeat(105);
-            const quizId = adminQuizCreate(authUserId, quizName, "");
-
-
-            // Update Quizes Description with invalid description
-            
-            expect(adminQuizDescriptionUpdate(authUserId, quizId, longDescription)).toStrictEqual(ERROR);
-
-        });
-
-    });
-
-
-    describe('Valid Input test', () => {
-
-        test('All inputs are valid', () => {
-
-            // First Create User
-
-            const email = 'sample@gmail.com';
-            const password = 'samplepassword1';
-            const nameFirst = 'firstname';
-            const nameLast = 'lastname';
-            const authUserId = adminAuthRegister(email, password, nameFirst, nameLast);
-            
-            // Create Quiz
-
-            const quizName = "randomquiz";
-            const newDescription = "newdescription";
-            const quizId = adminQuizCreate(authUserId, quizName, "");
-
-
-            // Update Quizes Description
-            
-            expect(adminQuizDescriptionUpdate(authUserId, quizId, newDescription)).toStrictEqual({});
-
-        });
-
-        test('All inputs are valid but description is empty string', () => {
-
-            // First Create User
-
-            const email = 'sample@gmail.com';
-            const password = 'samplepassword1';
-            const nameFirst = 'firstname';
-            const nameLast = 'lastname';
-            const authUserId = adminAuthRegister(email, password, nameFirst, nameLast);
-            
-            // Create Quiz
-
-            const quizName = "randomquiz";
-            const newDescription = "";
-            const quizId = adminQuizCreate(authUserId, quizName, "olddescription");
-
-
-            // Update Quizes Description
-            
-            expect(adminQuizDescriptionUpdate(authUserId, quizId, newDescription)).toStrictEqual({});
-
-        });
-
-    });
-
 
   });
-  
+
+  describe('Invalid Input Tests', () => {
+
+    test('authUserId is invalid but all other variables are correct', () => {
+        
+      expect(adminQuizDescriptionUpdate(authUserId.authUserId + 1, quizId.quizId, "")).toStrictEqual(ERROR);
+        
+    });
+
+
+    test('Invalid quizId but all other variables correct', () => {
+        
+      expect(adminQuizDescriptionUpdate(authUserId.authUserId, quizId.quizId + 1, "")).toStrictEqual(ERROR);
+
+    });
+
+    test('given user inputs quizId they dont own but all other variables correct', () => {
+
+      const authUserId2 = adminAuthRegister("testing@gmail.com", "badpassword44", "Testing", "testing");
+
+      const quizId2 = adminQuizCreate(authUserId2.authUserId, "randomquiz", "");
+        
+      expect(adminQuizDescriptionUpdate(authUserId.authUserId, quizId2.quizId, "")).toStrictEqual(ERROR);
+
+    });
+
+
+    test('Invalid Description i.e description.length > 100 characters but all other variables correct', () => {
+
+      const longDescription = "a".repeat(105);
+        
+      expect(adminQuizDescriptionUpdate(authUserId.authUserId, quizId.quizId, longDescription)).toStrictEqual(ERROR);
+
+    });
+
+  });
+
+  describe('Valid Input test', () => {
+
+    test('All inputs are valid', () => {
+
+      expect(adminQuizDescriptionUpdate(authUserId.authUserId, quizId.quizId, "newDescription")).toStrictEqual({});
+
+    });
+
+    test('All inputs are valid but description is empty string', () => {
+        
+      expect(adminQuizDescriptionUpdate(authUserId, quizId, "")).toStrictEqual({});
+
+    });
+
+  });
+
+});
+
