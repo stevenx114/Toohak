325d5baa6aab33bc660b310382b7cfb9650bb1b1
Tiffany Zhu <z5481408@ad.unsw.edu.au>
2023-10-02 23:40:16 +1100

    Add tests for adminUserDetails in auth.test.js

--- src/auth.test.js
+++ src/auth.test.js
@@ -10,31 +10,34 @@ import {
 
 const ERROR = { error: expect.any(String) }
 
+// Tests for adminUserDetails function
 describe('adminUserDetails Test', () => {
     let user;
     let userDetails;
 
+    // Success cases for adminUserDetails function
     describe('Success Cases', () => {
-    beforeEach(() => {
-        clear();
-        user = adminAuthRegister('johnsmith@gmail.com', 'ilovecat123', 'John', 'Smith');
-        userDetails = adminUserDetails(user.authUserId);
+        beforeEach(() => {
+            clear();
+            user = adminAuthRegister('johnsmith@gmail.com', 'ilovecat123', 'John', 'Smith');
+            userDetails = adminUserDetails(user.authUserId);
+        });
+
+        test('Successful implementation', () => {        
+            expect(userDetails).toEqual({
+            user: {
+                userId: user.authUserId,
+                name: 'John Smith',
+                email: 'johnsmith@gmail.com',
+                numSuccessfulLogins: 1, 
+                numFailedPasswordsSinceLastLogin: 0, 
+            },
+            })
+        });
+
     });
 
-    test('Successful implementation', () => {        
-        expect(userDetails).toEqual({
-          user: {
-            userId: user.authUserId,
-            name: 'John Smith',
-            email: 'johnsmith@gmail.com',
-            numSuccessfulLogins: 1, 
-            numFailedPasswordsSinceLastLogin: 0, 
-          },
-        })
-    });
-
-    });
-
+    // Error cases for adminUserDetails function
     describe('Error cases', () => {
         test('AuthUserId is not a valid user', () => {
             clear();
