ec9c02155263ed451608011ed06ea5713410a46b
Tiffany Zhu <z5481408@ad.unsw.edu.au>
2023-10-03 20:48:56 +1100

    Fix field name error for adminUserDetails tests in auth.test.js

--- src/auth.test.js
+++ src/auth.test.js
@@ -20,13 +20,13 @@ describe('adminUserDetails Test', () => {
         beforeEach(() => {
             clear();
             user = adminAuthRegister('johnsmith@gmail.com', 'ilovecat123', 'John', 'Smith');
-            userDetails = adminUserDetails(user.authUserId);
+            userDetails = adminUserDetails(user.userId);
         });
 
         test('Successful implementation', () => {        
             expect(userDetails).toEqual({
             user: {
-                userId: user.authUserId,
+                userId: user.userId,
                 name: 'John Smith',
                 email: 'johnsmith@gmail.com',
                 numSuccessfulLogins: 1, 
@@ -41,7 +41,7 @@ describe('adminUserDetails Test', () => {
     describe('Error cases', () => {
         test('AuthUserId is not a valid user', () => {
             clear();
-            expect(adminUserDetails(user.authUserId).toEqual(ERROR));
+            expect(adminUserDetails(user.userId).toEqual(ERROR));
         });
     });
 }); 
