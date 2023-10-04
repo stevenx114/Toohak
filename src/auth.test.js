f5ea11d2c8d39631d7ea63ce9cf6714f1ebcd181
Steven Xu <z5481988@ad.unsw.edu.au>
2023-10-04 18:31:21 +1100

    Fixed bugs in tests for adminAuthRegister in auth.test.js

--- src/auth.test.js
+++ src/auth.test.js
@@ -17,36 +17,36 @@ beforeEach(() => {
 
 // Success and fail tests for adminAuthRegister
 describe('Tests for adminAuthRegister', () => {
-    test('Fails on duplicate email', () => {
-        let newUserId = adminAuthRegister('hello@gmail.com', 'password', 'hello', 'world');
-        expect(adminAuthRegister('hello@gmail.com', 'password', 'hello', 'world')).toEqual({ error: expect.any(String) });
+    test('Existing email', () => {
+        adminAuthRegister('hello@gmail.com', 'password1', 'hello', 'world');
+        expect(adminAuthRegister('hello@gmail.com', 'password1', 'hello', 'world')).toEqual({ error: expect.any(String) });
     });
-    test('Fails on invalid email', () => {
+    test('Invalid email', () => {
         expect(adminAuthRegister('hello', 'password1', 'hello', 'world')).toEqual({ error: expect.any(String) }); 
     });
-    test('Fails on invalid name', () => {
+    test('First name has forbidden characters', () => {
         expect(adminAuthRegister('hello@gmail.com', 'password1', 'hello!', 'world')).toEqual({ error: expect.any(String) }); 
     });
-    test('Fails on invalid first name length', () => {
+    test('First name too short', () => {
         expect(adminAuthRegister('hello@gmail.com', 'password1', 'h', 'world')).toEqual({ error: expect.any(String) }); 
     });
-    test('Fails on invalid first name length', () => {
+    test('First name too long', () => {
         expect(adminAuthRegister('hello@gmail.com', 'password1', 'hellohellohellohellohello', 'world')).toEqual({ error: expect.any(String) }); 
     });
-    test('Fails on invalid last name', () => {
+    test('Last name has forbidden characters', () => {
         expect(adminAuthRegister('hello@gmail.com', 'password1', 'hello', 'world!')).toEqual({ error: expect.any(String) }); 
     });
-    test('Fails on invalid last name length', () => {
+    test('Last name too short', () => {
         expect(adminAuthRegister('hello@gmail.com', 'password1', 'hello', 'w')).toEqual({ error: expect.any(String) }); 
     });
-    test('Fails on invalid last name length', () => {
+    test('Last name too long', () => {
         expect(adminAuthRegister('hello@gmail.com', 'password1', 'hello', 'worldworldworldworldworld')).toEqual({ error: expect.any(String) }); 
     });
-    test('Fails if password is less than 8 characters', () => {
-        expect(adminAuthRegister('hello@gmail.com', 'pass', 'hello', 'worldworldworldworldworld')).toEqual({ error: expect.any(String) }); 
+    test('Password less than 8 characters', () => {
+        expect(adminAuthRegister('hello@gmail.com', 'pass', 'hello', 'world')).toEqual({ error: expect.any(String) }); 
     });
-    test('Fails if password does not contain at least one number and at least one letter', () => {
-        expect(adminAuthRegister('hello@gmail.com', 'password', 'hello', 'worldworldworldworldworld')).toEqual({ error: expect.any(String) });
+    test('Password does not contain at least one number and at least one letter', () => {
+        expect(adminAuthRegister('hello@gmail.com', 'password', 'hello', 'world')).toEqual({ error: expect.any(String) });
     });
     test('Valid email, password, first name and last name', () => {
         expect(adminAuthRegister('hello@gmail.com', 'password1', 'hello', 'world')).toEqual({ authUserId: expect.any(Number) });
