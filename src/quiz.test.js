dbbdee88f893b7f8f9d09cd872345f18c8440f27
Steven Xu <z5481988@ad.unsw.edu.au>
2023-10-06 10:07:36 +1100

    Fix conflicts in tests in quiz.test.js

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -1,17 +1,17 @@
-import {
-    adminQuizNameUpdate, 
-    adminQuizCreate,
-    adminQuizInfo,
-    adminQuizList
-} from './quiz';
-
-import {
-    adminAuthRegister,
-    adminAuthLogin
+import {    
+    adminAuthRegister, 
+    adminAuthLogin,
 } from './auth';
 
 import {
-    clear
+    adminQuizInfo,
+    adminQuizCreate,
+    adminQuizList,
+    adminQuizNameUpdate, 
+} from './quiz';
+
+import {
+    clear,
 } from './other';
 
 const ERROR = { error: expect.any(String) };
@@ -19,61 +19,6 @@ const ERROR = { error: expect.any(String) };
 beforeEach(() => {
     clear();
 });
-
-// Tests for adminQuizNameUpdate function
-describe('adminQuizNameUpdate', () => {
-    let newUser;
-    let newQuiz;
-    beforeEach(() => {
-        newUser = adminAuthRegister('johnsmith@gmail.com', 'ilovedog123', 'John', 'Smith');
-        newQuiz = adminQuizCreate(newUser.authUserId, 'nameNew', 'descriptionNew');
-    });
-
-    // Success cases for adminQuizNameUpdate function
-    describe('Success Cases', () => {
-        test('Successful implementation', () => {        
-            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'nameUpdated')).toEqual({});
-            const curQuiz = adminQuizInfo(newUser.authUserId, newQuiz.quizId);
-            expect(curQuiz.name).toEqual('nameUpdated');
-        });
-    });
-
-    // Error cases for adminQuizNameUpdate function
-    describe('Error cases', () => {
-        test('AuthUserId is not a valid user', () => {
-            clear();
-            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'nameUpdated')).toEqual(ERROR); 
-        });
-
-        test('Quiz ID does not refer to a valid quiz', () => {
-            clear();
-            newUser = adminAuthRegister('johnsmith@gmail.com', 'ilovedog123', 'John', 'Smith');
-            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'nameUpdated')).toEqual(ERROR);
-        });
-
-        test('Quiz ID does not refer to a quiz that this user owns', () => {
-            const noQuizzesUser = adminAuthRegister('george@gmail.com', 'ilovedog123', 'George', 'Smith');
-            expect(adminQuizNameUpdate(noQuizzesUser.authUserId, newQuiz.quizId, 'nameUpdated')).toEqual(ERROR);
-        });
-
-        test('Name contains invalid characters. Valid characters are alphanumeric and spaces', () => {
-            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'name Updated!')).toEqual(ERROR); 
-        });
-
-        test('Name is less than 3 characters long', () => {
-            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'na')).toEqual(ERROR); 
-        });
-
-        test('Name is more than 30 characters long', () => {
-            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'nameUpdatednameUpdatednameUpdated')).toEqual(ERROR); 
-        });
-
-        test('Name is already used by the current logged in user for another quiz', () => {
-            adminQuizCreate(newUser.authUserId, 'nameOther', 'descriptionNew');
-            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'nameOther')).toEqual(ERROR);
-        });
-    });
-}); 
 
 // Tests for adminQuizInfo function
 describe('adminQuizInfo Test', () => {
@@ -167,3 +112,58 @@ describe('AdminQuizCreate', () => {
         });
     });
 });
+
+// Tests for adminQuizNameUpdate function
+describe('adminQuizNameUpdate', () => {
+    let newUser;
+    let newQuiz;
+    beforeEach(() => {
+        newUser = adminAuthRegister('johnsmith@gmail.com', 'ilovedog123', 'John', 'Smith');
+        newQuiz = adminQuizCreate(newUser.authUserId, 'nameNew', 'descriptionNew');
+    });
+
+    // Success cases for adminQuizNameUpdate function
+    describe('Success Cases', () => {
+        test('Successful implementation', () => {        
+            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'nameUpdated')).toEqual({});
+            const curQuiz = adminQuizInfo(newUser.authUserId, newQuiz.quizId);
+            expect(curQuiz.name).toEqual('nameUpdated');
+        });
+    });
+
+    // Error cases for adminQuizNameUpdate function
+    describe('Error cases', () => {
+        test('AuthUserId is not a valid user', () => {
+            clear();
+            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'nameUpdated')).toEqual(ERROR); 
+        });
+
+        test('Quiz ID does not refer to a valid quiz', () => {
+            clear();
+            newUser = adminAuthRegister('johnsmith@gmail.com', 'ilovedog123', 'John', 'Smith');
+            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'nameUpdated')).toEqual(ERROR);
+        });
+
+        test('Quiz ID does not refer to a quiz that this user owns', () => {
+            const noQuizzesUser = adminAuthRegister('george@gmail.com', 'ilovedog123', 'George', 'Smith');
+            expect(adminQuizNameUpdate(noQuizzesUser.authUserId, newQuiz.quizId, 'nameUpdated')).toEqual(ERROR);
+        });
+
+        test('Name contains invalid characters. Valid characters are alphanumeric and spaces', () => {
+            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'name Updated!')).toEqual(ERROR); 
+        });
+
+        test('Name is less than 3 characters long', () => {
+            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'na')).toEqual(ERROR); 
+        });
+
+        test('Name is more than 30 characters long', () => {
+            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'nameUpdatednameUpdatednameUpdated')).toEqual(ERROR); 
+        });
+
+        test('Name is already used by the current logged in user for another quiz', () => {
+            adminQuizCreate(newUser.authUserId, 'nameOther', 'descriptionNew');
+            expect(adminQuizNameUpdate(newUser.authUserId, newQuiz.quizId, 'nameOther')).toEqual(ERROR);
+        });
+    });
+}); 
