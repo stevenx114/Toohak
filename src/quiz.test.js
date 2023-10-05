2e768bc2cf633df64a7ac850952a87dd7bfb8a76
Yiru Jang <z5480998@ad.unsw.edu.au>
2023-10-05 20:48:14 +1100

    Completed testing for adminQuizCreate in quiz.test.js

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -3,66 +3,55 @@ import { adminAuthRegister, adminAuthLogin } from './auth';
 import { clear } from './other';
 
 const ERROR = {error: expect.any(String)};
+
+beforeEach(() => {
+    clear();
+});
+
+describe('AdminQuizCreate', () => {
+    let userId;
+    let quiz;
     beforeEach(() => {
-        clear();    
+        userId = adminAuthRegister('voxekov792@estudys.com', 'quickbrown1', 'Alex', 'Smith');
+        quiz = adminQuizCreate(userId.authUserId, 'Australia', 'description');
     });
 
-    describe('AdminQuizCreate', () => {
-        // Error cases for AdminQuizCreate function
-        let userId;
-        let quizzes;
-        describe('error cases:', () => {
-            
-            beforeEach(() => {
-                userId = adminAuthRegister('voxekov792@estudys.com', 'quickbrown', 'Alex', 'Smith');
-                quizzes = adminQuizList(userId.authUserId);
-            });
-            
-            test('invalid AuthUserId', () => {
-                clear();
-                expect(adminQuizCreate(userId.authUserId, 'human history', 'description')).toStrictEqual(ERROR);
-            })
+    // Error cases for AdminQuizCreate function
+    test('invalid authUserId', () => {
+        clear();
+        expect(adminQuizCreate(userId.authUserId, 'human history', 'description')).toStrictEqual(ERROR);
+    });
 
-            test.each([
-                { name: ''}, // empty
-                { name: '!23'}, // invalid char
-                { name: '!*^&'}, // invalid char
-                { name: 'qw'}, // < 2 chars
-                { name: '12'}, // < 2 chars
-                { name: 'qwerty'.repeat(6)}, // > 30 chars
-            ])('invalid name input: $name', ({ name }) => {
-                expect(adminQuizCreate(userId.authUserId, name, 'description')).toStrictEqual(ERROR);
-            });
+    test.each([
+        { name: ''}, // empty
+        { name: '!23'}, // invalid char
+        { name: '!*^&'}, // invalid char
+        { name: 'qw'}, // < 2 chars
+        { name: '12'}, // < 2 chars
+        { name: 'qwerty'.repeat(6)}, // > 30 chars
+    ])('invalid name input: $name', ({ name }) => {
+        expect(adminQuizCreate(userId.authUserId, name, 'description')).toStrictEqual(ERROR);
+    });
 
-            // test('existing quiz name', () => {
-            //     expect(adminQuizCreate(userId.authUserId, quizzes.name, 'description')).toStrictEqual({error: "quiz name is already in use"});
-            // });
+    const longDescription = 'description'.repeat(10);
+    test('description too long', () => {
+        expect(adminQuizCreate(userId.authUserId, 'games', longDescription)).toStrictEqual(ERROR);
+    });
 
-            test('existing quiz name', () => {
-                expect(adminQuizCreate(userId.authUserId, quizzes.name, 'description')).toStrictEqual(ERROR);
-            });
+    test('existing quiz name', () => {
+        expect(adminQuizCreate(userId.authUserId, 'Australia', 'description')).toStrictEqual(ERROR);
+    });
 
-            const longDescription = 'description'.repeat(10);
-            test('description too long', () => {
-                expect(adminQuizCreate(userId.authUserId, 'games', longDescription)).toStrictEqual(ERROR);
-            });
-
-        });
-
-        // Success case for adminQuizCreate function
-        describe('success case:', () => {
-            let userId;
-            test('Valid input', () => {
-                userId = adminAuthRegister('voxekov792@estudys.com', 'quickbrown', 'Alex', 'Smith');
-                expect(adminQuizCreate(userId.authUserId, 'human history', 'description')).toStrictEqual({
-                    quizId: expect.any(Number),
-                });
-            });
+    // Success case for adminQuizCreate function
+    test('Valid input', () => {
+        expect(adminQuizCreate(userId.authUserId, 'human history', 'description')).toStrictEqual({
+            quizId: expect.any(Number),
         });
     });
 
-    
-
-
-
-
+    test('Valid input, description empty', () => {
+        expect(adminQuizCreate(userId.authUserId, 'human history', '')).toStrictEqual({
+            quizId: expect.any(Number),
+        });
+    });
+});
