92b53c239fdff466613d5427574488a55fe182bd
Yiru Jang <z5480998@ad.unsw.edu.au>
2023-10-06 12:16:41 +1100

    Updated tests for adminQuizList in quiz.test.js

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -112,3 +112,48 @@ describe('AdminQuizCreate', () => {
         });
     });
 });
+
+describe('AdminQuizList', () => {
+    let userId;
+    let quiz;
+    let quiz1;
+    beforeEach(() => {
+        userId = adminAuthRegister('voxekov792@estudys.com', 'quickbrown1', 'Alex', 'Smith');
+        quiz = adminQuizCreate(userId.authUserId, 'human history', 'description');
+    });
+
+    // error case
+    test('invalid authUserId', () => {
+        clear();
+        expect(adminQuizList(userId.authUserId)).toStrictEqual(ERROR);
+    });
+
+    // success cases:
+    test('valid input of 1 quiz', () => {
+        expect(adminQuizList(userId.authUserId)).toStrictEqual({
+            quizzes: [
+                {
+                    quizId: quiz.quizId,
+                    name: 'human history',
+                }
+            ]
+        });
+    });
+
+    test('valid input of 2 quizzes', () => {
+        quiz1 = adminQuizCreate(userId.authUserId, 'animals', 'description');
+        expect(adminQuizList(userId.authUserId)).toStrictEqual({
+            quizzes: [
+                {
+                    quizId: quiz.quizId,
+                    name: 'human history',
+                },
+                {
+                    quizId: quiz1.quizId,
+                    name: 'animals',
+                },
+            ]
+        });
+    });
+
+});
