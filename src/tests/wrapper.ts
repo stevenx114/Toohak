660b99700e405970fc9889b1c5762a361104dce7
Tiffany Zhu <z5481408@ad.unsw.edu.au>
2023-10-24 22:25:56 +1100

    Fixed linting for wrapper.ts

--- src/tests/wrapper.ts
+++ src/tests/wrapper.ts
@@ -1,9 +1,6 @@
 import request from 'sync-request-curl';
 import config from '../src/config.json';
-import { MessageSend, QuestionInput } from '../src/types';
 
-const OK = 200;
-const INPUT_ERROR = 400;
 const port = config.port;
 const url = config.url;
 
@@ -11,109 +8,109 @@ const SERVER_URL = `${url}:${port}`;
 
 // Wrapper for adminQuizCreate
 export function requestQuizCreateV1(token: string, name: string, description: string) {
-    return request(
-        'POST',
-        SERVER_URL + '/v1/admin/quiz', 
-        {
-            json: {
-                token: token,
-                name: name,
-                description: description,
-            }
-        }
-    );
+  return request(
+    'POST',
+    SERVER_URL + '/v1/admin/quiz',
+    {
+      json: {
+        token: token,
+        name: name,
+        description: description,
+      }
+    }
+  );
 }
 
 // Wrapper for adminQuizList
 export function requestQuizListV1(token: string) {
-    return request(
-        'GET',
-        SERVER_URL + '/v1/admin/quiz/list',
-        {
-            qs: {
-                token: token,
-            }
-        }
-    );
+  return request(
+    'GET',
+    SERVER_URL + '/v1/admin/quiz/list',
+    {
+      qs: {
+        token: token,
+      }
+    }
+  );
 }
 
 // Wrapper for adminQuizRemove
 export function requestQuizRemoveV1(quizid: number, token: string) {
-    return request(
-        'DELETE',
-        SERVER_URL + '/v1/admin/quiz/' + quizid,
-        {
-            qs: {
-                token: token,
-            }
-        }
-    );
+  return request(
+    'DELETE',
+    SERVER_URL + '/v1/admin/quiz/' + quizid,
+    {
+      qs: {
+        token: token,
+      }
+    }
+  );
 }
 
 // Wrapper for adminQuizDescriptionUpdate
 export function requestQuizDescriptionUpdateV1(quizid: number, token: string, description: string) {
-    return request(
-        'PUT',
-        SERVER_URL + '/v1/admin/quiz/' + quizid + '/description',
-        {
-            json: {
-                token: token,
-                description: description,
-            }
-        }
-    );
+  return request(
+    'PUT',
+    SERVER_URL + '/v1/admin/quiz/' + quizid + '/description',
+    {
+      json: {
+        token: token,
+        description: description,
+      }
+    }
+  );
 }
 
 // Wrapper for adminQuizNameUpdate
 export function requestQuizNameUpdateV1(quizid: number, token: string, name: string) {
-    return request(
-        'PUT',
-        SERVER_URL + '/v1/admin/quiz/' + quizid + '/name',
-        {
-            json: {
-                token: token,
-                name: name,
-            }
-        }
-    );
+  return request(
+    'PUT',
+    SERVER_URL + '/v1/admin/quiz/' + quizid + '/name',
+    {
+      json: {
+        token: token,
+        name: name,
+      }
+    }
+  );
 }
 
 // Wrapper for adminQuizInfo
 export function requestQuizInfo(quizid: number, token: string) {
-    return request(
-        'GET',
-        SERVER_URL + '/v1/admin/quiz/' + quizid,
-        {
-            qs: {
-                token: token,
-            }
-        }
-    );
+  return request(
+    'GET',
+    SERVER_URL + '/v1/admin/quiz/' + quizid,
+    {
+      qs: {
+        token: token,
+      }
+    }
+  );
 }
 
 // Wrapper for adminAuthRegister
 export function requestAuthRegister(email: string, password: string, nameFirst: string, nameLast: string) {
-    return request(
-        'POST',
-        SERVER_URL + '/v1/admin/auth/register',
-        {
-            json: {
-                email: email,
-                password: password,
-                nameFirst: nameFirst,
-                nameLast: nameLast,
-            }
-        }
-    );
+  return request(
+    'POST',
+    SERVER_URL + '/v1/admin/auth/register',
+    {
+      json: {
+        email: email,
+        password: password,
+        nameFirst: nameFirst,
+        nameLast: nameLast,
+      }
+    }
+  );
 }
 
 // Wrapper for clear
 export function requestClearV1() {
-    return request(
-      'DELETE',
-      SERVER_URL + '/v1/clear',
-      {
-        json: {}
-      }
-    );
-  }
+  return request(
+    'DELETE',
+    SERVER_URL + '/v1/clear',
+    {
+      json: {}
+    }
+  );
+}
