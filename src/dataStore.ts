777c5eabb329b82d19f89f252a914b8e974a29ec
Steven Xu <z5481988@ad.unsw.edu.au>
2023-10-26 03:24:55 +1100

    Added quiz trash array in DataStore interface in dataStore.ts

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,4 +1,18 @@
-// YOU SHOULD MODIFY THIS OBJECT BELOW
+export interface Answer {
+  answerId: number;
+  answer: string;
+  colour: string;
+  correct: boolean;
+}
+
+export interface Question {
+  questionId: number;
+  question: string;
+  duration: number;
+  points: number,
+  answers: Answer[];
+}
+
 export interface User {
   userId: number;
   name: string;
@@ -24,47 +38,19 @@ export interface Token {
   authUserId: number;
 }
 
-export interface Question {
-  questionId: number;
-  question: string;
-  duration: number;
-  points: number,
-  answers: Answer[];
-}
-
-export interface Answer {
-    answerId: number;
-    answer: string;
-    colour: string;
-    correct: boolean;
-}
-
 export interface DataStore {
   users: User[];
   quizzes: Quiz[];
   tokens: Token[];
+  trash: Quiz[];
 }
 
 let data: DataStore = {
   users: [],
   quizzes: [],
-  tokens: []
+  tokens: [],
+  trash: []
 };
-// YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
-
-/*
-Example usage
-    let store = getData()
-    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Rando'] }
-
-    names = store.names
-
-    names.pop()
-    names.push('Jake')
-
-    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Jake'] }
-    setData(store)
-*/
 
 // Use get() to access the data
 export const getData = (): DataStore => data;
