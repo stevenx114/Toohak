415b2d9f4601b86c41a75a9dc3a9cb0671e555d6
Steven Xu <z5481988@ad.unsw.edu.au>
2023-10-27 07:11:57 +1100

    Merged files from master

--- src/tests/wrapper.ts
+++ src/tests/wrapper.ts
@@ -30,14 +30,6 @@ export function requestUserDetails(token: string) {
   return JSON.parse(res.body.toString());
 }
 
-import request from 'sync-request-curl';
-import config from '../config.json';
-
-const port = config.port;
-const url = config.url;
-
-const SERVER_URL = `${url}:${port}`;
-
 // Wrapper for adminQuizNameUpdate
 export function requestQuizNameUpdate(token: string, quizid: number, name: string) {
   const res = request('PUT', SERVER_URL + '/v1/admin/quiz/' + quizid + '/name', {
