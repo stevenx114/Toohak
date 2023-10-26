04806dffa2d50410e5463e43a0200b82793dd1bc
Tiffany Zhu <z5481408@ad.unsw.edu.au>
2023-10-27 01:13:32 +1100

    Updated dataStore.ts for iteration 2

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,4 +1,18 @@
-// YOU SHOULD MODIFY THIS OBJECT BELOW
+export interface Answer {
+  answerId: number;
+  answer: string;
+  colour: string;
+  correct: boolean;
+}
+
+export interface Question {
+  questionId: number;
+  question: string;
+  duration: number;
+  points: number,
+  answers: Answer[];
+}
+
 export interface User {
   userId: number;
   name: string;
@@ -15,33 +29,29 @@ export interface Quiz {
   timeCreated: number;
   timeLastEdited: number;
   description: string;
+  numQuestions?: number;
+  questions?: Question[];
+  duration?: number;
+}
+
+export interface Token {
+  sessionId: string;
+  authUserId: number;
 }
 
 export interface DataStore {
   users: User[];
   quizzes: Quiz[];
+  tokens?: Token[];
+  trash?: Quiz[];
 }
 
 let data: DataStore = {
   users: [],
   quizzes: [],
+  tokens: [],
+  trash: [],
 };
-
-// YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
-
-/*
-Example usage
-    let store = getData()
-    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Rando'] }
-
-    names = store.names
-
-    names.pop()
-    names.push('Jake')
-
-    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Jake'] }
-    setData(store)
-*/
 
 // Use get() to access the data
 export const getData = (): DataStore => data;
