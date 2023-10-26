61bb4020023c9339eb21755b4b0ab0489c59e9d1
Steven Xu <z5481988@ad.unsw.edu.au>
2023-10-27 07:07:47 +1100

    Edited implementation of adminAuthLogout tests in adminAuthLogout.test.ts

--- src/tests/adminAuthLogout.test.ts
+++ src/tests/adminAuthLogout.test.ts
@@ -20,13 +20,17 @@ describe('POST /v1/admin/auth/logout', () => {
   let errorReturn: ErrorObject;
   let userToken: TokenReturn;
   describe('Success Cases', () => {
-    userToken = requestAuthRegister(validDetails.EMAIL, validDetails.PASSWORD, validDetails.FIRST_NAME, validDetails.LAST_NAME);
-    expect(requestLogout(userToken.token)).toEqual({});
+    test('All inputs are valid', () => {
+      userToken = requestAuthRegister(validDetails.EMAIL, validDetails.PASSWORD, validDetails.FIRST_NAME, validDetails.LAST_NAME);
+      expect(requestLogout(userToken.token)).toEqual({});
+    });
   });
 
   describe('Error Cases', () => {
-    errorReturn = requestLogout('');
-    expect(errorReturn.error).toEqual(ERROR);
-    expect(errorReturn.statusCode).toEqual(401);
+    test('Token is invalid', () => {
+      errorReturn = requestLogout('');
+      expect(errorReturn.error).toEqual(ERROR);
+      expect(errorReturn.statusCode).toEqual(401);
+    });
   });
 });
