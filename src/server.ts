d48de956cde9530dec94e7d037739dd1c5b124df
Kai <z5481984@ad.unsw.edu.au>
2023-10-27 12:49:42 +1100

    Implemented function adminQuizRemove to include trash functionality in quiz.ts

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,8 @@ import {
 import {
   adminQuizInfo,
   adminQuizCreate,
-  adminQuizList
+  adminQuizList,
+  adminQuizRemove
 } from './quiz';
 
 // Set up web app
@@ -107,6 +108,19 @@ app.post('/v1/admin/quiz', (req: Request, res: Response) => {
   if ('error' in result) {
     return res.status(result.statusCode).json(result);
   }
+  res.json(result);
+});
+
+// adminQuizRemove
+app.delete('/v1/admin/quiz/:quizid', (req: Request, res: Response) => {
+  const quizId = parseInt(req.params.quizid);
+  const token = req.query.token as string;
+  const result = adminQuizRemove(token, quizId);
+
+  if ('error' in result) {
+    return res.status(result.statusCode).json(result);
+  }
+
   res.json(result);
 });
 
