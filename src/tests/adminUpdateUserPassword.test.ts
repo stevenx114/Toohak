bc18cf26b19339759de0a61a60085a6f90973b5b
Kai <z5481984@ad.unsw.edu.au>
2023-10-27 18:21:43 +1100

    Added new tests to test adminUserUpdatePassword without userDetails function

--- src/tests/adminUpdateUserPassword.test.ts
+++ src/tests/adminUpdateUserPassword.test.ts
@@ -7,40 +7,38 @@ import {
   import {
     requestClear,
     requestAuthRegister,
-    requestUserDetails,
     requestAdminUpdateUserPassword,
+    requestAuthLogin,
+    requestUserDetails,
+    requestLogout
   } from './wrapper';
   
   const ERROR = expect.any(String);
 
-  beforeEach(() => {
-    requestClear();
-    let token; 
-  });
+  let token: TokenReturn; 
+  let errorResult: ErrorObject;
 
   describe("Tests for adminUpdateUserPassword", () => {
-    let errorResult: ErrorObject;
+    beforeEach(() => {
+      requestClear();
+      token = requestAuthRegister(validDetails.EMAIL, validDetails.PASSWORD, validDetails.FIRST_NAME, validDetails.LAST_NAME);
+    });
     const newPassword = "newPassword1";
     const newerPassword = "newPassword2";
-    describe("Success Case", () => {    
-      token = requestAuthRegister(validDetails.EMAIL, validDetails.PASSWORD, validDetails.FIRST_NAME, validDetails.LAST_NAME);
-      requestAdminUpdateUserPassword(token, validDetails.PASSWORD, newPassword);
-      expect(requestUserDetails(token).user.password).toEqual(newPassword);
-      requestAdminUpdateUserPassword(token, newPassword, newerPassword);
-      expect(requestUserDetails(token).user.password).toEqual(newerPassword);
-      expect(requestUserDetails(token).user.previousPasswords).toEqual([validDetails.PASSWORD, newPassword]);
-      expect(requestAdminUpdateUserPassword(token, newerPassword, newPassword).error).toEqual(expect.any(string));
-      expect(requestAdminUpdateUserPassword(token, newerPassword, newPassword).statusCode).toEqual(400);
+    describe("Success Case", () => {   
+      test("Success Case", () => {
+        console.log(requestAdminUpdateUserPassword(token.token, validDetails.PASSWORD, newPassword));
+        console.log(requestAuthLogin(validDetails.EMAIL, newPassword));
+      })  
     })
     describe ("Error Cases", () => {
-      requestClear()
       token = requestAuthRegister(validDetails.EMAIL, validDetails.PASSWORD, validDetails.FIRST_NAME, validDetails.LAST_NAME);
       test.each([
-        [token, "samplePassword1", newPassword],
-        [token, validDetails.PASSWORD, validDetails.PASSWORD],
-        [token, validDetails.PASSWORD, "abcdef1"],
-        [token, validDetails.PASSWORD, "abcdefgh"],
-        [token, validDetails.PASSWORD, "12345678"],
+        [token.token, "samplePassword1", newPassword],
+        [token.token, validDetails.PASSWORD, validDetails.PASSWORD],
+        [token.token, validDetails.PASSWORD, "abcdef1"],
+        [token.token, validDetails.PASSWORD, "abcdefgh"],
+        [token.token, validDetails.PASSWORD, "12345678"],
       ])('%s', (token, oldPassword, newPassword) => {
         errorResult = requestAdminUpdateUserPassword(token, oldPassword, newPassword);
         expect(errorResult.error).toEqual(ERROR);
