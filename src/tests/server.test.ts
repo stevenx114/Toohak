6331ec520204c2f567b5668475c8ffc40af1ca1b
Steven Xu <z5481988@ad.unsw.edu.au>
2023-11-13 01:19:05 +1100

    Added test for 404 error to improve test coverage in server.test.ts

---
+++ src/tests/server.test.ts
@@ -0,0 +1,8 @@
+import {
+  requestNonExistentRoute
+} from './wrapper'
+import HTTPError from 'http-errors';
+
+test('Success test for error 404', () => {
+  expect(() => requestNonExistentRoute()).toThrow(HTTPError[404]);
+});
