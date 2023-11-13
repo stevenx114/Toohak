fe445bfcaa70bd3e5926eb84ed397f3d9c540944
Yiru Jang <z5480998@ad.unsw.edu.au>
2023-11-13 22:23:39 +1100

    Written http tests for adminQuizListV2 in adminQuizListV2.test.ts

---
+++ src/tests/adminQuizListV2.test.ts
@@ -0,0 +1,72 @@
+import {
+    validDetails,
+    QuizIdReturn,
+    TokenReturn,
+} from '../types';
+  
+import {
+  requestAuthRegister,
+  requestQuizCreate,
+  requestClear,
+  requestQuizList,
+  requestLogout
+} from './wrapper';
+  
+import HTTPError from 'http-errors';
+
+beforeEach(() => {
+  requestClear();
+});
+  
+// Tests for adminQuizList
+describe('GET /v1/admin/quiz/list', () => {
+  let user: TokenReturn;
+  let quiz: QuizIdReturn;
+  let quiz1: QuizIdReturn;
+  beforeEach(() => {
+    user = requestAuthRegisterV2(validDetails.EMAIL, validDetails.PASSWORD, validDetails.FIRST_NAME, validDetails.LAST_NAME);
+    quiz = requestQuizCreateV2(user.token, validDetails.QUIZ_NAME, validDetails.DESCRIPTION);
+  });
+
+  // Error cases
+  test('Token is empty', () => {
+    expect(() => requestQuizListV2('')).toThrow(HTTPError[401]);
+  });
+
+  test('Token is invalid', () => {
+    expect(() => requestQuizListV2(user.token + 1)).toThrow(HTTPError[401]);
+  });
+
+  test('Token does not refer to valid logged in user session', () => {
+    requestLogoutV2(user.token);
+    expect(() => requestQuizListV2(user.token)).toThrow(HTTPError[401]);
+  });
+
+  // success cases:
+  test('valid input of 1 quiz', () => {
+    expect(requestQuizListV2(user.token)).toStrictEqual({
+      quizzes: [
+        {
+          quizId: quiz.quizId,
+          name: validDetails.QUIZ_NAME,
+        }
+      ]
+    });
+  });
+
+  test('valid input of 2 quizzes', () => {
+    quiz1 = requestQuizCreateV2(user.token, validDetails.QUIZ_NAME_2, validDetails.DESCRIPTION);
+    expect(requestQuizListV2(user.token)).toStrictEqual({
+      quizzes: [
+        {
+          quizId: quiz.quizId,
+          name: validDetails.QUIZ_NAME,
+        },
+        {
+          quizId: quiz1.quizId,
+          name: validDetails.QUIZ_NAME_2,
+        },
+      ]
+    });
+  });
+});
