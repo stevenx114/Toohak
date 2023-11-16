84a2a35b4b4ea4a4182eb39e6fd2c1c303a9e11b
Tiffany Zhu <z5481408@ad.unsw.edu.au>
2023-11-16 21:42:37 +1100

    Fixed linting in server.ts and tests/v2adminQuizInfo.test.ts

--- src/server.ts
+++ src/server.ts
@@ -168,7 +168,6 @@ app.put('/v1/admin/quiz/:quizid/name', (req: Request, res: Response) => {
   res.json(adminQuizNameUpdate(token, quizId, name));
 });
 
-
 // adminQuizNameUpdateV2
 app.put('/v2/admin/quiz/:quizid/name', (req: Request, res: Response) => {
   const token = req.headers.token as string;
@@ -304,9 +303,6 @@ app.post('/v2/admin/quiz/:quizid/question/:questionid/duplicate', (req: Request,
   const questionId = parseInt(req.params.questionid);
   res.json(adminQuizQuestionDuplicate(token, quizId, questionId));
 });
-
-
-
 
 // ====================================================================
 //  ================= WORK IS DONE ABOVE THIS LINE ===================
--- src/tests/v2adminQuizInfo.test.ts
+++ src/tests/v2adminQuizInfo.test.ts
@@ -1,61 +1,60 @@
 import {
-    validDetails,
-    QuizIdReturn,
-    TokenReturn,
-  } from '../types';
-  
-  import {
-    requestAuthRegister,
-    requestQuizInfoV2,
-    requestClear,
-    requestQuizCreateV2
-  } from './wrapper';
-  
-  import HTTPError from 'http-errors';
-  
+  validDetails,
+  QuizIdReturn,
+  TokenReturn,
+} from '../types';
+
+import {
+  requestAuthRegister,
+  requestQuizInfoV2,
+  requestClear,
+  requestQuizCreateV2
+} from './wrapper';
+
+import HTTPError from 'http-errors';
+
+beforeEach(() => {
+  requestClear();
+});
+
+// Tests for adminQuizInfo function
+describe('GET /v2/admin/quiz/{quizid}', () => {
+  let ownsQuizUserToken: TokenReturn;
+  let noQuizUserToken: TokenReturn;
+  let quizId: QuizIdReturn;
   beforeEach(() => {
-    requestClear();
+    ownsQuizUserToken = requestAuthRegister(validDetails.EMAIL, validDetails.PASSWORD, validDetails.FIRST_NAME, validDetails.LAST_NAME);
+    noQuizUserToken = requestAuthRegister(validDetails.EMAIL_2, validDetails.PASSWORD_2, validDetails.FIRST_NAME_2, validDetails.LAST_NAME_2);
+    quizId = requestQuizCreateV2(ownsQuizUserToken.token, validDetails.QUIZ_NAME, validDetails.DESCRIPTION);
   });
-  
-  // Tests for adminQuizInfo function
-  describe('GET /v2/admin/quiz/{quizid}', () => {
-    let ownsQuizUserToken: TokenReturn;
-    let noQuizUserToken: TokenReturn;
-    let quizId: QuizIdReturn;
-    beforeEach(() => {
-      ownsQuizUserToken = requestAuthRegister(validDetails.EMAIL, validDetails.PASSWORD, validDetails.FIRST_NAME, validDetails.LAST_NAME);
-      noQuizUserToken = requestAuthRegister(validDetails.EMAIL_2, validDetails.PASSWORD_2, validDetails.FIRST_NAME_2, validDetails.LAST_NAME_2);
-      quizId = requestQuizCreateV2(ownsQuizUserToken.token, validDetails.QUIZ_NAME, validDetails.DESCRIPTION);
-    });
-  
-    // Success cases for adminQuizInfo function
-    describe('Success Cases', () => {
-      test.skip('Correct details', () => {
-        expect(requestQuizInfoV2(ownsQuizUserToken.token, quizId.quizId)).toStrictEqual({
-          quizId: quizId.quizId,
-          name: validDetails.QUIZ_NAME,
-          timeCreated: expect.any(Number),
-          timeLastEdited: expect.any(Number),
-          description: validDetails.DESCRIPTION,
-          numQuestions: expect.any(Number), 
-          questions: [],
-          duration: 0,
-          thumbnailUrl: expect.any(String)
-        });
-      });
-    });
-  
-    // Error cases for adminQuizInfo function
-    describe('Error cases', () => {
-      test('Token is empty', () => {
-        expect(() => requestQuizInfoV2('', quizId.quizId)).toThrow(HTTPError[401]);
-      });
-      test('Token is invalid', () => {
-        expect(() => requestQuizInfoV2(ownsQuizUserToken.token + 1, quizId.quizId)).toThrow(HTTPError[401]);
-      });
-      test('Quiz ID does not refer to a quiz that this user owns', () => {
-        expect(() => requestQuizInfoV2(noQuizUserToken.token, quizId.quizId)).toThrow(HTTPError[403]);
+
+  // Success cases for adminQuizInfo function
+  describe('Success Cases', () => {
+    test.skip('Correct details', () => {
+      expect(requestQuizInfoV2(ownsQuizUserToken.token, quizId.quizId)).toStrictEqual({
+        quizId: quizId.quizId,
+        name: validDetails.QUIZ_NAME,
+        timeCreated: expect.any(Number),
+        timeLastEdited: expect.any(Number),
+        description: validDetails.DESCRIPTION,
+        numQuestions: expect.any(Number),
+        questions: [],
+        duration: 0,
+        thumbnailUrl: expect.any(String)
       });
     });
   });
-  
+
+  // Error cases for adminQuizInfo function
+  describe('Error cases', () => {
+    test('Token is empty', () => {
+      expect(() => requestQuizInfoV2('', quizId.quizId)).toThrow(HTTPError[401]);
+    });
+    test('Token is invalid', () => {
+      expect(() => requestQuizInfoV2(ownsQuizUserToken.token + 1, quizId.quizId)).toThrow(HTTPError[401]);
+    });
+    test('Quiz ID does not refer to a quiz that this user owns', () => {
+      expect(() => requestQuizInfoV2(noQuizUserToken.token, quizId.quizId)).toThrow(HTTPError[403]);
+    });
+  });
+});
