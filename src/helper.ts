de0b5ef580a2b086c4a8c6cb5bd13a356bb79ceb
Steven Xu <z5481988@ad.unsw.edu.au>
2023-11-17 03:42:35 +1100

    Added helper functions for adminQuizSessionStateUpdate in helper.ts

--- src/helper.ts
+++ src/helper.ts
@@ -3,8 +3,18 @@ import {
   Quiz,
   User,
   Question,
-  Token
+  Token,
+  Session,
+  getTimerData,
+  setTimerData,
+  Timer,
+  setData
 } from './dataStore';
+
+import {
+  SessionState,
+  SessionAction
+} from './types';
 
 import crypto from 'crypto';
 
@@ -33,6 +43,117 @@ export const getQuestion = (quizId: number, questionId: number): Question | unde
   return quiz.questions.find(q => q.questionId === questionId);
 };
 
-export function getHashOf(password: string) {
+export const getSession = (sessionId: number): Session | undefined => {
+  const data = getData();
+  return data.sessions.find(session => session.sessionId === sessionId);
+};
+
+export const getHashOf = (password: string): string => {
   return crypto.createHash('sha256').update(password).digest('hex');
-}
+};
+
+export const sleepSync = (ms: number) => {
+  const startTime = new Date().getTime();
+  while (new Date().getTime() - startTime < ms) {
+    // zzzZZ - comment needed so eslint doesn't complain
+  }
+};
+
+export const isValidAction = (state: string, action: string): boolean => {
+  if (state === SessionState.LOBBY) {
+    if (action === SessionAction.SKIP_COUNTDOWN ||
+        action === SessionAction.GO_TO_ANSWER ||
+        action === SessionAction.GO_TO_FINAL_RESULTS) {
+      return false;
+    }
+  } else if (state === SessionState.QUESTION_COUNTDOWN) {
+    if (action === SessionAction.NEXT_QUESTION ||
+        action === SessionAction.GO_TO_ANSWER ||
+        action === SessionAction.GO_TO_FINAL_RESULTS) {
+      return false;
+    }
+  } else if (state === SessionState.QUESTION_OPEN) {
+    if (action === SessionAction.NEXT_QUESTION ||
+        action === SessionAction.SKIP_COUNTDOWN ||
+        action === SessionAction.GO_TO_FINAL_RESULTS) {
+      return false;
+    }
+  } else if (state === SessionState.QUESTION_CLOSE) {
+    if (action === SessionAction.SKIP_COUNTDOWN) {
+      return false;
+    }
+  } else if (state === SessionState.ANSWER_SHOW) {
+    if (action === SessionAction.SKIP_COUNTDOWN ||
+        action === SessionAction.GO_TO_ANSWER) {
+      return false;
+    }
+  } else if (state === SessionState.FINAL_RESULTS) {
+    if (action === SessionAction.NEXT_QUESTION ||
+        action === SessionAction.SKIP_COUNTDOWN ||
+        action === SessionAction.GO_TO_ANSWER ||
+        action === SessionAction.GO_TO_FINAL_RESULTS) {
+      return false;
+    }
+  } else if (state === SessionState.END) {
+    return false;
+  }
+  return true;
+};
+
+const startCountdown = (sessionId: number) => {
+  const data = getData();
+  const timerData: Timer[] = getTimerData();
+  const curSession = getSession(sessionId);
+  const timeoutId: ReturnType<typeof setTimeout> = setTimeout(() => {
+    curSession.state = SessionState.QUESTION_OPEN;
+    setData(data);
+  }, 3000);
+  const newTimer: Timer = {
+    timeoutId: timeoutId,
+    sessionId: sessionId
+  };
+  timerData.push(newTimer);
+  setTimerData(timerData);
+};
+
+const clearCountdown = (sessionId: number) => {
+  const timerData: Timer[] = getTimerData();
+  const timer = timerData.find(timer => timer.sessionId === sessionId);
+  clearTimeout(timer.timeoutId);
+  const indexOfTimer = timerData.findIndex(timer => timer.sessionId === sessionId);
+  timerData.splice(indexOfTimer, 1);
+  setTimerData(timerData);
+};
+
+export const getNextState = (sessionId: number, state: string, action: string): string => {
+  let newState: string = state;
+
+  if (action === SessionAction.END) {
+    newState = SessionState.END;
+  } else if (state === SessionState.LOBBY && action === SessionAction.NEXT_QUESTION) {
+    newState = SessionState.QUESTION_COUNTDOWN;
+    startCountdown(sessionId);
+  } else if (state === SessionState.QUESTION_COUNTDOWN && action === SessionAction.SKIP_COUNTDOWN) {
+    clearCountdown(sessionId);
+    newState = SessionState.QUESTION_OPEN;
+  } else if (state === SessionState.QUESTION_OPEN && action === SessionAction.GO_TO_ANSWER) {
+    newState = SessionState.ANSWER_SHOW;
+  } else if (state === SessionState.QUESTION_CLOSE) {
+    if (action === SessionAction.GO_TO_ANSWER) {
+      newState = SessionState.ANSWER_SHOW;
+    } else if (action === SessionAction.NEXT_QUESTION) {
+      newState = SessionState.QUESTION_COUNTDOWN;
+      startCountdown(sessionId);
+    } else if (action === SessionAction.GO_TO_FINAL_RESULTS) {
+      newState = SessionState.FINAL_RESULTS;
+    }
+  } else if (state === SessionState.ANSWER_SHOW) {
+    if (action === SessionAction.NEXT_QUESTION) {
+      newState = SessionState.QUESTION_COUNTDOWN;
+      startCountdown(sessionId);
+    } else if (action === SessionAction.GO_TO_FINAL_RESULTS) {
+      newState = SessionState.FINAL_RESULTS;
+    }
+  }
+  return newState;
+};
